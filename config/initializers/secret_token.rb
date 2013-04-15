38aa217e8225aaa28c7be9fc1ccec8dff1ba333a
Ali <alistair.edmunds@gmail.com>
2013-04-15 13:31:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tommisooni::Application.config.secret_token = 'b014c2e49c551597fb1313e305e75beef4d05e7e745bdeff541692f8a265ef623975edfa9d4388938fed8b1338e1e693d767c4da47405e1f50c7181cdd767475'
